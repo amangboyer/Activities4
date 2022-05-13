3cf7e34b5e9b9b2aab8314f6da8bf5acc9b930f7
amangboyer <67471199+amangboyer@users.noreply.github.com>
2022-05-12 23:10:56 -0400

    Add files via upload

---
+++ src/sample/Controller.java
@@ -0,0 +1,13 @@
+
+package sample;
+        import javafx.event.ActionEvent;
+
+        import javafx.scene.control.TextField;
+public class Controller {
+    private TextField Title;
+
+    void btnOKClicked(ActionEvent event){
+    }
+}
+
+
